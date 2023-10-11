a692a606e102b1e76c09c846cdd67d8eae7568e9
Benjamin Labrecque <labrecque.benji@gmail.com>
2023-10-11 12:39:09 -0300

    Add basic two sum version in lua

---
+++ two_sum.lua
@@ -0,0 +1,62 @@
+-- http://lua-users.org/wiki/FileInputOutput
+
+-- see if the file exists
+function file_exists(file)
+  local f = io.open(file, "rb")
+  if f then f:close() end
+  return f ~= nil
+end
+
+-- get all lines from a file, returns an empty 
+-- list/table if the file does not exist
+function lines_from(file)
+  if not file_exists(file) then return {} end
+  local lines = {}
+  for line in io.lines(file) do 
+    lines[#lines + 1] = line
+  end
+  return lines
+end
+
+-- tests the functions above
+local file = 'two_sum_input.txt'
+local lines = lines_from(file)
+
+-- print all line numbers and their contents
+--[[ for k,v in pairs(lines) do ]]
+--[[   print('line[' .. k .. ']', v) ]]
+--[[ end ]]
+
+function addToSet(set, key)
+    set[key] = true
+end
+
+function removeFromSet(set, key)
+    set[key] = nil
+end
+
+function setContains(set, key)
+    return set[key] ~= nil
+end
+
+function two_sum(nums_str, target)
+    --[[ print(nums) ]]
+    --[[ print(target) ]]
+    seen = {}
+
+    nums = {}
+    for num in nums_str:gmatch("%S+") -- match all non-space characters
+        do table.insert(nums, tonumber(num))
+    end
+
+    for _, num in ipairs(nums) do
+        complement = target - num
+        if setContains(seen, complement) then
+            print(complement, num)
+        else
+            addToSet(seen, num)
+        end
+    end
+end
+
+two_sum(lines[1], lines[2])
