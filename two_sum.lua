2c9237da074c62a7614b56fd98fbc8c21195777f
Benjamin Labrecque <labrecque.benji@gmail.com>
2023-10-16 13:06:39 -0300

    extract file utils

--- two_sum.lua
+++ two_sum.lua
@@ -1,33 +1,9 @@
 hashset = require("hashset")
-
--- http://lua-users.org/wiki/FileInputOutput
--- see if the file exists
-function file_exists(file)
-  local f = io.open(file, "rb")
-  if f then f:close() end
-  return f ~= nil
-end
-
--- get all lines from a file, returns an empty 
--- list/table if the file does not exist
-function lines_from(file)
-  if not file_exists(file) then return {} end
-  local lines = {}
-  for line in io.lines(file) do 
-    lines[#lines + 1] = line
-  end
-  return lines
-end
+utils_files = require("utils_files")
 
 -- tests the functions above
 local file = 'two_sum_input.txt'
-local lines = lines_from(file)
-
--- print all line numbers and their contents
---[[ for k,v in pairs(lines) do ]]
---[[   print('line[' .. k .. ']', v) ]]
---[[ end ]]
-
+local lines = utils_files.lines_from(file)
 
 function two_sum(nums_str, target)
     --[[ print(nums) ]]
